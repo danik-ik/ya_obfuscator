d1915d8dd401d3c42813f179a88cd07f2f506355
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2016-07-07 12:14:46 +0300

    Добавил assert, переместил node_modules для тестов в папку test

--- test/maintest.js
+++ test/maintest.js
@@ -1,13 +1,15 @@
 /**
  * Created by danik-ik on 07.07.16.
  */
+var assert = require('assert');
 
 describe ('Самый главный тест', function () {
 	it('should pass', function(done) {
+		assert.deepEqual([], []);
 		done();
 	});
 	it('should not pass', function(done) {
-		throw('don`t pass');
+		assert.equal([], []);
 		done();
 	});
 })
